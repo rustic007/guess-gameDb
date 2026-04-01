dc4ce68c110af77f86d5ab3c30d77d14b957d233
Ruslan <ruslanospanov0306@gmail.com>
2026-04-01 05:24:20 -0400

    feat: added random num

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo "Enter your username: "
-read USERNAME
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+RANDOM_NUM=$(shuf -i 1-1000 -n 1)
 
-echo "Hello $USERNAME"
+
+echo "Enter your username: "
+read USER
+
+echo "Hello $USER"
+
+
+USERNAME=$($PSQL "SELECT $USER FROM user_info")
+
+if [[ -z $USERNAME ]]; then
+  echo "Welcome, $USER! It looks like this is your first time here."
+  
+fi
