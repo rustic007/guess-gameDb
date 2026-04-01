585b47f445a56adfbf29d92dee0da7265057a9c5
Ruslan <ruslanospanov0306@gmail.com>
2026-04-01 06:50:34 -0400

    fix: fixed the best_game column problem

--- number_guess.sh
+++ number_guess.sh
@@ -6,19 +6,17 @@ RANDOM_NUM=$(shuf -i 1-1000 -n 1)
 echo "Enter your username: "
 read USER
 
-echo "Hello $USER"
-
-# Parse pipe-separated output correctly
+# Query user information from database
 QUERY_RESULT=$($PSQL "SELECT username, best_game, game_played FROM user_info WHERE username = '$USER';")
 
-# Split by pipe delimiter
+# Parse pipe-separated output (format: username|best_game|game_played)
 IFS='|' read -r USERNAME BEST_GAME GAMES_PLAYED <<< "$QUERY_RESULT"
 
-if [[ $USERNAME ]]; then
+if [[ -n $USERNAME ]]; then
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 else
   echo "Welcome, $USER! It looks like this is your first time here."
-  $PSQL "INSERT INTO user_info (username) VALUES ('$USER');" > /dev/null
+  $PSQL "INSERT INTO user_info (username, game_played) VALUES ('$USER', 0);" > /dev/null
 fi
 
 echo "Guess the secret number between 1 and 1000:"
@@ -28,8 +26,14 @@ NUMBER_OF_GUESS=0
 while true; do
   read USER_GUESS
   
+  # Check if input is a valid positive integer
+  if ! [[ $USER_GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+  
   if [[ $USER_GUESS -gt $RANDOM_NUM ]]; then
-    echo "It's lower than that, guess again: "
+    echo "It's lower than that, guess again:"
     ((NUMBER_OF_GUESS += 1))
   elif [[ $USER_GUESS -lt $RANDOM_NUM ]]; then
     echo "It's higher than that, guess again:"
@@ -38,15 +42,14 @@ while true; do
     ((NUMBER_OF_GUESS += 1))
     echo "You guessed it in $NUMBER_OF_GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
     break
-  else 
-    echo "That is not an integer, guess again:" 
-    ((NUMBER_OF_GUESS += 1))
   fi
 done
 
 # Update database with final result
+# If best_game is empty (first game) or current game is better, update best_game
 if [[ -z $BEST_GAME ]] || [[ $NUMBER_OF_GUESS -lt $BEST_GAME ]]; then
   $PSQL "UPDATE user_info SET best_game = $NUMBER_OF_GUESS, game_played = game_played + 1 WHERE username = '$USER';" > /dev/null
 else
+  # Just increment games played if this wasn't the best game
   $PSQL "UPDATE user_info SET game_played = game_played + 1 WHERE username = '$USER';" > /dev/null
 fi
